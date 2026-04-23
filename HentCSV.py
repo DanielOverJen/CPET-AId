9b8fe13cfee2597ecf1a0f2b208931a3c257556c
Daniel Overgaard <doje23@student.aau.dk>
2026-04-23 11:13:06 +0200

    Tilføjelse af SE's hent CSV-fil

---
+++ HentCSV.py
@@ -0,0 +1,49 @@
+import numpy as np 
+import pandas as pd
+
+####Hent af data ####
+
+df = pd.read_csv("CPET_ProcessedData.csv")
+
+
+# patient_ids = df.iloc[:, 0]   # gem ID (til SHAP senere)
+
+df = df.iloc[:,3:] #Fjerner de 2 første søjler med patient ID og køn. Iloc gør at alle rækker starter fra søjle 3 og frem 
+
+
+#Vi har fjernet de første 3 søjler, så for at hente labels skal det være mellem 5 og 9.
+y = df.iloc[:, 5:9].values #søjler til supervised learning
+print(y)
+
+classification_names = df.columns[5:9] #henter søjlenavne
+print(classification_names)
+
+
+print(y.shape[0]) #Her printer vi antallet af observationer, som gerne burde være 217
+
+y = np.argmax(y, axis= 1) #Samler søjlerne i 1 søjle med 0,1,2,3 værdier.
+#0: hjerte, 1: Lunge, 2: muskulært/andet, 3: rask
+
+print(classification_names[y])
+
+
+# Features: resten af kolonnerne efter labels
+X = df.iloc[:, 9:].values
+
+feature_names = df.columns[9:]
+#Vi shuffler rækkerne og inddeler til træning og test
+
+np.random.seed(42)
+indices = np.random.permutation(len(X))
+
+X = X[indices] #Meget vigtigt at X og Y shuffles til de samme rækker. 
+y = y[indices]
+
+#80% til træning
+split = int(0.8 * len(X))
+
+X_train = X[:split]
+X_test = X[split:]
+
+y_train = y[:split]
+y_test = y[split:]
