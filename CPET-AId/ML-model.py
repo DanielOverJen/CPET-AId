0195958983d6ccd431444dc32ea3c083efca12f4
Søren Elias <selchristiansen@gmail.com>
2026-04-28 15:06:51 +0200

    Ml-model som er færdig og virker

--- CPET-AId/ML-model.py
+++ CPET-AId/ML-model.py
@@ -3,10 +3,17 @@ from xgboost import XGBClassifier
 CPETAIdModel = XGBClassifier()
 CPETAIdModel.load_model("CPET-AId/CPET-AId/CPETAId_model.json")
 
-data = [0.668427, 104.5, 1.416444, 2.031264, 0.754807, 0.177091]
 
-classification_names = ["Kardiel", "Pulmonalt", "Muskulært", "Rask"]
+def classify(data):
+    """Function to calculate probabilities for the four classes. The data input should be:
+    data = [DiffPercentPeakVO2, PeakVE, first_half_O2Slope, StdVEVCO2, O2PulsePercent, first_half_VO2Slope]
+    """
+    proba = CPETAIdModel.predict_proba([data])
+    
+    classification_names = ["Kardielt", "Pulmonalt", "Muskulært", "Rask"]
 
-proba = CPETAIdModel.predict_proba([data])
+    proba = proba[0]*100
 
-print(proba)
+    result = [(name, round(float(p), 1)) for name, p in zip(classification_names, proba)]
+
+    return result    
