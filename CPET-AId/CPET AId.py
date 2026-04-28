aab1cae9ccc47fe2411467a23aaa60f64fc01ec1
Daniel Overgaard <doje23@student.aau.dk>
2026-04-28 08:56:37 +0200

    fælles snak om input og output af de sidste funktioner

--- CPET-AId/CPET AId.py
+++ CPET-AId/CPET AId.py
@@ -5,6 +5,7 @@ def CPET_AId(data):
     if data_validation(pre_processed_data):
         CPET_AId_proba[] = ML_model(pre_processed_data)
         # eksempel på CPET_AId_proba["fys_name", probaility]
+        
         cardiac_post_processed, pulmo_post_processed, musco_post_processed, healthy_post_processed, R_validation = post_processing(ML_model) 
         # eksempel på et af disse array: cardiac_post_processed = {["Kardielt:", 50%, parameters[], Highest_value]}
         # parameters[(VO2_name,VO2_value),("PVO2",10%)]
@@ -13,4 +14,4 @@ def CPET_AId(data):
 
         PDF_print(barchart, cardiac_post_processed, pulmo_post_processed, musco_post_processed, healthy_post_processed, R_validation)
     else:
-        PDF_print(error)
+        PDF_print_error()
