0ab578a20442c20a05f5cc7f3d4fc70efc79085a
Louise Ebener <louiseebener@icloud.com>
2026-04-30 13:00:14 +0200

    Ændrede navn på CPET AId.py

--- CPET-AId/CPET_AId.py
+++ CPET-AId/CPET_AId.py
@@ -1,9 +1,11 @@
+import Datavalidering
+
 def CPET_AId(data):
     R = data[0]
     pre_processed_data = data[1:]
     
-    if data_validation(pre_processed_data):
-        CPET_AId_proba[] = ML_model(pre_processed_data)
+    if Datavalidering.Datavalidation(pre_processed_data):
+        CPET_AId_proba = ML_model(pre_processed_data)
         # eksempel på CPET_AId_proba["fys_name", probaility]
         
         cardiac_post_processed, pulmo_post_processed, musco_post_processed, healthy_post_processed, R_validation = post_processing(ML_model) 
