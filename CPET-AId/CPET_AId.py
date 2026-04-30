069bac958dfdd15dd7c5a3be09bb0ee05b3d2688
Louise Ebener <louiseebener@icloud.com>
2026-04-30 13:16:46 +0200

    Ændret inputs og outputs til postprocessering i CPET_AId.py

--- CPET-AId/CPET_AId.py
+++ CPET-AId/CPET_AId.py
@@ -1,5 +1,6 @@
 import Datavalidering
 import ML_model
+import Post_processering
 
 def CPET_AId(data):
     peak_R = data[0]
@@ -9,7 +10,7 @@ def CPET_AId(data):
         CPET_AId_proba = ML_model.classify(pre_processed_data)
         # eksempel på CPET_AId_proba[("fys_name", probability),x4]
         
-        cardiac_post_processed, pulmo_post_processed, musco_post_processed, healthy_post_processed, R_validation = Post_processing(pre_processed_data, CPET_AId_proba, peak_R) 
+        R_validation, feature_names_values, shap_values, base_values, global_base_values = Post_processering.post_processing(peak_R, pre_processed_data) 
         # eksempel på et af disse array: cardiac_post_processed = {["Kardielt:", 50%, parameters[], Highest_value]}
         # parameters[(VO2_name,VO2_value),("PVO2",10%)]
                 
